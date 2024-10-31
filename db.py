800f67108a3335541216fdc3ce96dfec61fc477b
shibil-thora <shibilallakkat123@gmail.com>
2024-10-31 17:55:09 +0530

    added basic setup created a table

--- db.py
+++ db.py
@@ -1,2 +1,14 @@
-print('launced') 
- 
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String  
+import os
+
+engine = create_engine(os.environ.get('SQL_CONNECT_URL'), echo=True) 
+meta = MetaData() 
+
+students = Table(
+    'students', meta, 
+    Column('id', Integer, primary_key=True), 
+    Column('name', String, index=True), 
+    Column('lastname', String),
+) 
+meta.create_all(engine)
+
