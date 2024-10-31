624ecfe3431bfe2abe58ef211dfba37900140e04
shibil-thora <shibilallakkat123@gmail.com>
2024-10-31 22:27:19 +0530

    select query in different ways

--- slct.py
+++ slct.py
@@ -1,7 +1,10 @@
 from db import students, conn 
+from sqlalchemy import select
 
 def run(): 
-    s = students.select() 
+    s = students.select().where(students.c.id > 11) # way 1
+    s = select(students.c.name, students.c.lastname).where(students.c.id > 11) # way 2
+    s = select(students).where(students.c.id==11) # way 3
     result = conn.execute(s) 
     print(result.fetchall())
 
--- where.py
+++
@@ -1,1 +0,0 @@
-print('where working')
