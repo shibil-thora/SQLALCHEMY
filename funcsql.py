c21264690fa5f657c49cef69ffa8ec4051ffa5bf
shibil-thora <shibilallakkat123@gmail.com>
2024-11-03 11:45:54 +0530

    added functions

---
+++ funcsql.py
@@ -0,0 +1,12 @@
+from sqlalchemy.sql import func  
+from db import conn, students, addresses
+from sqlalchemy import select
+
+def run(): 
+    query = select(students).where(students.c.id == select(func.max(students.c.id)).scalar_subquery()) 
+    query = select(func.min(students.c.id))
+    result = conn.execute(query) 
+    print(list(result))
+
+if __name__ == '__main__': 
+    run() 
--- logicals.py
+++ logicals.py
@@ -1,10 +1,11 @@
 from db import students, conn, addresses   
-from sqlalchemy import select, and_, or_, asc, desc 
+from sqlalchemy import select, and_, or_, asc, desc, between
 
 def run(): 
     query = students.select().where(and_(students.c.name == 'Ravi', students.c.lastname == 'Sattar')) 
     query = select(students.c.name, students.c.lastname).where(or_(students.c.name == 'Ravi', students.c.lastname == 'Sattar')) 
-    query = select(students.c.id).order_by(desc(students.c.id)) 
+    query = select(students.c.id).order_by(desc(students.c.id))  
+    query = select(students.c.id).where(between(students.c.id, 1, 3))
     result = conn.execute(query) 
     for row in result: 
         print(row)
